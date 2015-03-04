089ba7defb56f32367fca084fe33d5ae93f63dc2
Sable Cantus <sable@cantus.us>
2015-03-04 10:57:22 -0800

    updated web scripts

--- script_menu/2 - Add to Sites List.sh
+++ script_menu/2 - Add to Sites List.sh
@@ -3,8 +3,8 @@
 # add a url from the pasteboard to the sites.command
 
 pb=`pbpaste`
-url="open $pb"
-echo $url >> /Users/sable/Documents/sites.command
+url="$pb"
+echo $url >> /Users/sable/Library/Scripts/sites_list.txt
 
 osascript -e 'tell app "Finder" to display dialog "The URL has been added to your list."'
 
